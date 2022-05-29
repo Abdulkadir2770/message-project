341187ff2bab7cbac7ba27aeb82ad5fa4b3fb049
Amadi Y <absnacks17@gmail.com>
2022-05-29 16:47:17 -0500

    added the initial javascript scripts

---
+++ script.js
@@ -0,0 +1,9 @@
+const msg_display = document.getElementById("msg-display");
+const statement_holder = document.getElementById("statement-holder");
+const statement_one = document.getElementById("statement-one");
+const statement_two = document.getElementById("statement-two");
+const statement_three = document.getElementById("statement-three");
+const statement_four = document.getElementById("statement-four");
+
+
+console.log(msg_display, statement_holder, statement_one, statement_two, statement_three, statement_four);
